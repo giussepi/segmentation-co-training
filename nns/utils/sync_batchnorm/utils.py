af146b85657b09f4b971d8d738a1f9de4f5a5b96
giussepi <giussepexy@gmail.com>
2022-01-24 16:59:29 +0000

    nns/utils/sync_batchnorm/utils.py -> get_batchnorm2d_class modified to work only with the settings module

--- nns/utils/sync_batchnorm/utils.py
+++ nns/utils/sync_batchnorm/utils.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 """ nns/utils/sync_batchnorm/utils """
 
-from torch import nn
+import torch
+
+import settings
 from .batchnorm import SynchronizedBatchNorm2d
 
 
-def get_batchnorm2d_class(num_gpus):
+def get_batchnorm2d_class():
     """
     Returns the right BatchNorm2d based on the number of GPUS
 
-    Args:
-        num_gpus <int>: number of GPUS
-
     Returns:
-        nn.BatchNorm2d or SynchronizedBatchNorm2d
+        torch.nn.BatchNorm2d or SynchronizedBatchNorm2d
     """
-    assert isinstance(num_gpus, int), type(num_gpus)
+    # TODO: decide if the value from settings should be parameters or just imported
+    # from the settings module (like it is working right now)
+    if settings.CUDA == settings.MULTIGPUS == settings.PATCH_REPLICATION_CALLBACK == True and \
+       torch.cuda.is_available() and torch.cuda.device_count() > 1:
+        return SynchronizedBatchNorm2d
 
-    return nn.BatchNorm2d if num_gpus < 2 else SynchronizedBatchNorm2d
+    return torch.nn.BatchNorm2d
