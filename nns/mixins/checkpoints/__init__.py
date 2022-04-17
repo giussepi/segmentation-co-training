8523aa955c8afbac7f8832f53191383c77e1c8a5
giussepi <giussepexy@gmail.com>
2022-04-17 14:41:43 +0100

    nns/mixins/checkpoints/ missing module

---
+++ nns/mixins/checkpoints/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+""" nns/mixins/checkpoints/__init__ """
+
+from nns.mixins.checkpoints.standard_checkpoints import CheckPointMixin
+from nns.mixins.checkpoints.da_checkpoints import DACheckPointMixin
