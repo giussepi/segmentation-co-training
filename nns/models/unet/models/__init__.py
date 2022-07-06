1bab8cd54b5cf37af7a06f2622232c0f10eaf5b8
giussepi <giussepexy@gmail.com>
2022-07-06 19:43:05 +0100

    nns/models/unet -> models from Attention Gated Networks repsitory

---
+++ nns/models/unet/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from nns.models.unet.models.unet2d import UNet2D
+from nns.models.unet.models.unet_grid_attention import UNet_Grid_Attention
+from nns.models.unet.models.unet_att_dsv import UNet_Att_DSV, SingleAttentionBlock, \
+    MultiAttentionBlock
---
+++ nns/models/unet/utils.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+nns/models/unet/utils
+
+Source: https://github.com/ozan-oktay/Attention-Gated-Networks/blob/master/models/networks/utils.py
+"""
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from nns.models.unet.network_others import init_weights
+
+
+class unetConv2(nn.Module):
+    def __init__(self, in_size, out_size, is_batchnorm, n=2, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.n = n
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        s = stride
+        p = padding
+        if is_batchnorm:
+            for i in range(1, n+1):
+                conv = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, s, p),
+                                     nn.BatchNorm2d(out_size),
+                                     nn.ReLU(inplace=True),)
+                setattr(self, 'conv%d' % i, conv)
+                in_size = out_size
+
+        else:
+            for i in range(1, n+1):
+                conv = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, s, p),
+                                     nn.ReLU(inplace=True),)
+                setattr(self, 'conv%d' % i, conv)
+                in_size = out_size
+
+        # initialise the blocks
+        for m in self.children():
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs):
+        x = inputs
+        for i in range(1, self.n+1):
+            conv = getattr(self, 'conv%d' % i)
+            x = conv(x)
+
+        return x
+
+
+class UnetGridGatingSignal(nn.Module):
+    def __init__(self, in_size, out_size, kernel_size=1, is_batchnorm=True):
+        super().__init__()
+
+        if is_batchnorm:
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, stride=1, padding=0),
+                                       nn.BatchNorm2d(out_size),
+                                       nn.ReLU(inplace=True),
+                                       )
+        else:
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, stride=1, padding=0),
+                                       nn.ReLU(inplace=True),
+                                       )
+
+        # initialise the blocks
+        for m in self.children():
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        return outputs
+
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size, is_deconv, is_batchnorm=True):
+        super().__init__()
+        # self.conv = unetConv2(in_size, out_size, False)  # original
+        self.conv = unetConv2(in_size, out_size, is_batchnorm)  # modified following unetup3
+
+        if is_deconv:
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=4, stride=2, padding=1)
+        else:
+            self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+        # initialise the blocks
+        for m in self.children():
+            if m.__class__.__name__.find('unetConv2') != -1:
+                continue
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs1, inputs2):
+        outputs2 = self.up(inputs2)
+        offset = outputs2.size()[2] - inputs1.size()[2]
+        padding = 2 * [offset // 2, offset // 2]
+        outputs1 = F.pad(inputs1, padding)
+        return self.conv(torch.cat([outputs1, outputs2], 1))
+
+
+class UnetUp_CT(nn.Module):
+    def __init__(self, in_size, out_size, is_batchnorm=True):
+        super().__init__()
+        self.conv = unetConv2(in_size + out_size, out_size, is_batchnorm, kernel_size=3, padding=1)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear')
+
+        # initialise the blocks
+        for m in self.children():
+            if m.__class__.__name__.find('unetConv2') != -1:
+                continue
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs1, inputs2):
+        outputs2 = self.up(inputs2)
+        offset = outputs2.size()[2] - inputs1.size()[2]
+        padding = 2 * [offset // 2, offset // 2, 0]
+        outputs1 = F.pad(inputs1, padding)
+        return self.conv(torch.cat([outputs1, outputs2], 1))
+
+
+class UnetDsv(nn.Module):
+    def __init__(self, in_size, out_size, scale_factor):
+        super().__init__()
+        self.dsv = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size=1, stride=1, padding=0),
+                                 nn.Upsample(scale_factor=scale_factor, mode='bilinear'), )
+
+    def forward(self, x):
+        return self.dsv(x)
