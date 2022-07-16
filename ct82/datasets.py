c77df8fdcd88120508b236fe3c91e1b18e000d1f
giussepi <giussepexy@gmail.com>
2022-07-16 16:53:31 +0100

    ct82/datasets.py -> CT82Dataset

---
+++ ct82/datasets.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+""" ct82/datasets """
+
+import os
+import re
+
+import numpy as np
+import torch
+from gtorch_utils.datasets.segmentation import DatasetTemplate
+from gutils.numpy_.numpy_ import scale_using_general_min_max_values
+
+from ct82.constants import DICOM_MAX_VAL, DICOM_MIN_VAL
+from ct82.images import NIfTI, ProNIfTI
+
+
+__all__ = ['CT82Dataset']
+
+
+class CT82Dataset(DatasetTemplate):
+    """
+    Dataset for the cT82 dataset created through
+    ct82.processors.CT82MGR->process
+
+    Usage:
+
+    train, val, test = CT82Dataset.get_subdatasets(
+        train_path='CT-82-Pro/train', val_path='CT-82-Pro/val', test_path='CT-82-Pro/test')
+
+    train_dataloader = DataLoader(
+        train,
+        num_workers=0,
+        batch_size=batch_size * num_gpus,
+        shuffle=run_mode == DB.TRAIN,
+        drop_last=run_mode == DB.TRAIN,
+        **SeedWorker(preserve_reproductibility=True)(),
+    )
+    """
+
+    NUM_CLASSES = 1
+
+    def __init__(self, **kwargs):
+        """
+        Initializes the object instance
+
+        Kwargs:
+            images_masks_path  <str>: path to the folder for containing images and masks
+            filename_reg       <str>: regular expression to get the index id from the crop filename.
+                                      Default r'^CT\_(?P<id>[\d]+).pro.nii.gz$'
+            mask_name_tpl      <str>: Template to build the label name using the id. Default 'label_{}.nii.gz'
+            cot_mask_name_tpl  <str>: Template to build the co-training label name using the id.
+                                      Default 'label_{}.cot.nii.gz'
+            cotraining        <bool>: If True the co-training masks are returned; otherwise, returns
+                                      ground truth masks. Default False
+            original_masks    <bool>: If original_masks == cotraining_mask == True, then both the original
+                                      and cotraining masks are returned.
+                                      Default False
+        """
+        self.images_masks_path = kwargs.get('images_masks_path')
+        self.filename_reg = kwargs.get('filename_reg', r'^CT\_(?P<id>[\d]+).pro.nii.gz$')
+        self.mask_name_tpl = kwargs.get('mask_name_tpl', 'label_{}.nii.gz')
+        self.cot_mask_name_tpl = kwargs.get('cot_mask_name_tpl', 'label_{}.cot.nii.gz')
+        self.cotraining = kwargs.get('cotraining', False)
+        self.original_masks = kwargs.get('original_masks', False)
+
+        assert isinstance(self.images_masks_path, str), type(self.images_masks_path)
+        assert os.path.isdir(self.images_masks_path), self.images_masks_path
+        assert isinstance(self.filename_reg, str), type(self.filename_reg)
+        assert isinstance(self.mask_name_tpl, str), type(self.mask_name_tpl)
+        assert isinstance(self.cot_mask_name_tpl, str), type(self.cot_mask_name_tpl)
+        assert isinstance(self.cotraining, bool), type(self.cotraining)
+        assert isinstance(self.original_masks, bool), type(self.original_masks)
+
+        self.pattern = re.compile(self.filename_reg)
+        self.image_list = [file_ for file_ in os.listdir(self.images_masks_path) if bool(self.pattern.fullmatch(file_))]
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def get_original_mask(self, idx):
+        """
+        Loads and returns the original mask of the image with index idx
+
+        Kwargs:
+            idx <int>: image index
+
+        Returns:
+            original_mask <NIfTI>
+        """
+        assert isinstance(idx, int), type(idx)
+
+        subject_id = self.pattern.fullmatch(self.image_list[idx]).groupdict()['id']
+
+        mask = NIfTI(os.path.join(
+            self.images_masks_path,
+            self.mask_name_tpl.format(subject_id)
+        ))
+
+        return mask
+
+    def get_image_and_mask_files(self, idx):
+        """
+        Loads the image and mask corresponding to the file at position idx in the image list.
+        Besides, both are properly formatted to be used by the neuronal network before
+        returning them.
+
+        Kwargs:
+            idx <int>: image index
+
+        Returns:
+            image <np.ndarray>, target_mask <np.ndarray>, '', '', co_training_mask_path <str>,
+            original_target_mask <np.ndarray or empty string>
+        """
+        assert isinstance(idx, int), type(idx)
+
+        image = ProNIfTI(os.path.join(self.images_masks_path, self.image_list[idx]))
+        cot_mask_path = original_mask = original_target_mask = ''
+
+        if self.cotraining:
+            subject_id = self.pattern.fullmatch(self.image_list[idx]).groupdict()['id']
+            cot_mask_path = os.path.join(
+                self.images_masks_path,
+                self.cot_mask_name_tpl.format(subject_id)
+            )
+
+            # the very first time the co-training files do not exits; thus,
+            # we use the ground truth masks
+            if os.path.isfile(cot_mask_path):
+                mask = ProNIfTI(cot_mask_path)
+            else:
+                mask = self.get_original_mask(idx)
+
+            if self.original_masks:
+                original_mask = self.get_original_mask(idx)
+        else:
+            mask = self.get_original_mask(idx)
+
+        assert image.shape == mask.shape, \
+            (f'Image and mask {idx} should be the same shape, but they are {image.shape} and ',
+             f'{mask.shape} respectively')
+
+        image = image.ndarray
+        mask = mask.ndarray
+
+        target_mask = np.zeros((*mask.shape, self.NUM_CLASSES), dtype=np.float32)
+        target_mask[..., 0] = (mask == 1).astype(np.float32)
+
+        if isinstance(original_mask, NIfTI):
+            original_mask = original_mask.ndarray
+            original_target_mask = np.zeros((*original_mask.shape, self.NUM_CLASSES), dtype=np.float32)
+            original_target_mask[..., 0] = (original_mask == 1).astype(np.float32)
+
+        return image, target_mask, '', '', cot_mask_path, original_target_mask
+
+    @staticmethod
+    def preprocess(img):
+        """
+        Preprocess the image and returns it
+
+        Args:
+            img <np.ndarray>:
+        Returns:
+            image <np.ndarray>
+        """
+        assert isinstance(img, np.ndarray), type(img)
+
+        if len(img.shape) == 3:
+            # working with 3D CT, Rows*Cols*Slice to Slice*Rows*Cols
+            img = img.transpose(2, 0, 1)
+            img = scale_using_general_min_max_values(
+                img, min_val=DICOM_MIN_VAL, max_val=DICOM_MAX_VAL, feats_range=(0, 1))
+        else:
+            # working with 3D mask, Rows*Cols*Slice*Class to Class*Slice*Rows*Cols*
+            img = img.transpose(3, 2, 0, 1)
+
+        # the transforms should happen here
+        # transforms = {'train': ..., 'valtest': ...}
+
+        return img
+
+    def __getitem__(self, idx):
+        """
+        Gets the image and mask for the index idx and returns them
+
+        Kwargs:
+            idx <int>: image index
+
+        Returns:
+            dict(
+                image=<Tensor>, mask=<Tensor>, label=<int> label_name=<str>, updated_mask_path <str>,
+                original_mask <Tensor or empty str>
+            )
+        """
+        assert isinstance(idx, int), type(idx)
+
+        image, mask, label, label_name, updated_mask_path, original_mask = self.get_image_and_mask_files(idx)
+        image = self.preprocess(image)
+        mask = self.preprocess(mask)
+
+        if isinstance(original_mask, np.ndarray):
+            original_mask = self.preprocess(original_mask)
+            original_mask = torch.from_numpy(original_mask).type(torch.FloatTensor)
+
+        return {
+            'image': torch.from_numpy(image).type(torch.FloatTensor),
+            'mask': torch.from_numpy(mask).type(torch.FloatTensor),
+            'label': label,
+            'label_name': label_name,
+            'updated_mask_path': updated_mask_path,
+            'original_mask': original_mask
+        }
+
+    @classmethod
+    def get_subdatasets(cls, **kwargs):
+        """
+        Creates and returns train, validation and test subdatasets to be used with DataLoaders
+
+        Kwargs:
+            train_path         <str>: path to the folder for containing training images and masks
+            val_path           <str>: path to the folder for containing validation images and masks
+            test_path          <str>: path to the folder for containing testing images and masks. Default ''
+            filename_reg       <str>: regular expression to get the index id from the crop filename.
+                                      Default r'(?P<filename>\d+).ann.tiff'
+            image_extension    <str>: image extension. Default '.ann.tiff'
+            mask_extension     <str>: mask extension. Default '.mask.png'
+            cot_mask_extension <str>: co-traning mask extension. Default '.cot.mask.png'
+            cotraining        <bool>: If True the co-training masks are be returned; otherwise, returns
+                                      ground truth masks. Default False
+        Returns:
+           train <OfflineCoNSePDataset>, validation <OfflineCoNSePDataset>, test <OfflineCoNSePDataset or None>
+        """
+        train_path = kwargs.pop('train_path')
+        val_path = kwargs.pop('val_path')
+        test_path = kwargs.pop('test_path', '')
+
+        # making sure this keyword argument is not passed when creating the instances
+        if 'images_masks_path' in kwargs:
+            kwargs.pop('images_masks_path')
+
+        train_dataset = cls(images_masks_path=train_path, **kwargs)
+        val_dataset = cls(images_masks_path=val_path, **kwargs)
+        test_dataset = cls(images_masks_path=test_path, **kwargs) if test_path else None
+
+        return train_dataset, val_dataset, test_dataset
