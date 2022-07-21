8d59d28afc309cbec4bea97574005b7e0b4436f7
giussepi <giussepexy@gmail.com>
2022-07-21 17:10:03 +0100

    nns/models/unet/models/unet3d.py -> UNet3D

---
+++ nns/models/unet/models/unet3d.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+""" nns/models/unet/models/unet3d """
+
+import torch.nn.functional as F
+from torch import nn
+
+from nns.models.unet.network_others import init_weights
+from nns.models.unet.utils import UnetConv3, UnetUp3_CT
+
+
+__all__ = ['UNet3D']
+
+
+class UNet3D(nn.Module):
+    """
+    Original 3D UNet from Attention Gated Networks
+    source: https://github.com/ozan-oktay/Attention-Gated-Networks/blob/master/models/networks/unet_3D.py
+    """
+
+    def __init__(self, feature_scale=4, n_classes=21, n_channels=3, is_batchnorm=True):
+        super().__init__()
+        self.feature_scale = feature_scale
+        self.n_classes = n_classes
+        self.n_channels = n_channels
+        self.is_batchnorm = is_batchnorm
+
+        filters = [64, 128, 256, 512, 1024]
+        filters = [int(x / self.feature_scale) for x in filters]
+        maxpool_kernel_size = (2, 2, 2)
+
+        # downsampling
+        self.conv1 = UnetConv3(self.n_channels, filters[0], self.is_batchnorm)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=maxpool_kernel_size)
+
+        self.conv2 = UnetConv3(filters[0], filters[1], self.is_batchnorm)
+        self.maxpool2 = nn.MaxPool3d(kernel_size=maxpool_kernel_size)
+
+        self.conv3 = UnetConv3(filters[1], filters[2], self.is_batchnorm)
+        self.maxpool3 = nn.MaxPool3d(kernel_size=maxpool_kernel_size)
+
+        self.conv4 = UnetConv3(filters[2], filters[3], self.is_batchnorm)
+        self.maxpool4 = nn.MaxPool3d(kernel_size=maxpool_kernel_size)
+
+        self.center = UnetConv3(filters[3], filters[4], self.is_batchnorm)
+
+        # upsampling
+        self.up_concat4 = UnetUp3_CT(filters[4], filters[3], is_batchnorm)
+        self.up_concat3 = UnetUp3_CT(filters[3], filters[2], is_batchnorm)
+        self.up_concat2 = UnetUp3_CT(filters[2], filters[1], is_batchnorm)
+        self.up_concat1 = UnetUp3_CT(filters[1], filters[0], is_batchnorm)
+
+        # final conv (without any concat)
+        self.final = nn.Conv3d(filters[0], self.n_classes, 1)
+
+        # initialise weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                init_weights(m, init_type='kaiming')
+            elif isinstance(m, nn.BatchNorm3d):
+                init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs):
+        conv1 = self.conv1(inputs)
+        maxpool1 = self.maxpool1(conv1)
+
+        conv2 = self.conv2(maxpool1)
+        maxpool2 = self.maxpool2(conv2)
+
+        conv3 = self.conv3(maxpool2)
+        maxpool3 = self.maxpool3(conv3)
+
+        conv4 = self.conv4(maxpool3)
+        maxpool4 = self.maxpool4(conv4)
+
+        center = self.center(maxpool4)
+        up4 = self.up_concat4(conv4, center)
+        up3 = self.up_concat3(conv3, up4)
+        up2 = self.up_concat2(conv2, up3)
+        up1 = self.up_concat1(conv1, up2)
+
+        final = self.final(up1)
+
+        return final
+
+    @staticmethod
+    def apply_argmax_softmax(pred):
+        log_p = F.softmax(pred, dim=1)
+
+        return log_p
--- nns/models/unet/utils.py
+++ nns/models/unet/utils.py
@@ -6,10 +6,16 @@ Source: https://github.com/ozan-oktay/Attention-Gated-Networks/blob/master/model
 """
 
 import torch
-from torch import nn
 import torch.nn.functional as F
+from torch import nn
 
 from nns.models.unet.network_others import init_weights
+
+
+__all__ = [
+    'unetConv2', 'UnetGridGatingSignal',  'unetUp', 'UnetUp_CT', 'UnetDsv', 'UnetConv3',
+    'UnetUp3_CT'
+]
 
 
 class unetConv2(nn.Module):
@@ -125,3 +131,57 @@ class UnetDsv(nn.Module):
 
     def forward(self, x):
         return self.dsv(x)
+
+
+class UnetConv3(nn.Module):
+    def __init__(
+            self, in_size, out_size, is_batchnorm, kernel_size=(3, 3, 3), padding_size=(1, 1, 1),
+            init_stride=(1, 1, 1)
+    ):
+        super().__init__()
+
+        if is_batchnorm:
+            self.conv1 = nn.Sequential(nn.Conv3d(in_size, out_size, kernel_size, init_stride, padding_size),
+                                       nn.BatchNorm3d(out_size),
+                                       nn.ReLU(inplace=True),)
+            self.conv2 = nn.Sequential(nn.Conv3d(out_size, out_size, kernel_size, 1, padding_size),
+                                       nn.BatchNorm3d(out_size),
+                                       nn.ReLU(inplace=True),)
+        else:
+            self.conv1 = nn.Sequential(nn.Conv3d(in_size, out_size, kernel_size, init_stride, padding_size),
+                                       nn.ReLU(inplace=True),)
+            self.conv2 = nn.Sequential(nn.Conv3d(out_size, out_size, kernel_size, 1, padding_size),
+                                       nn.ReLU(inplace=True),)
+
+        # initialise the blocks
+        for m in self.children():
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+
+class UnetUp3_CT(nn.Module):
+    def __init__(
+            self, in_size, out_size, is_batchnorm=True, kernel_size=(3, 3, 3), padding_size=(1, 1, 1),
+            scale_factor=(2, 2, 2)
+    ):
+        super().__init__()
+        self.conv = UnetConv3(
+            in_size + out_size, out_size, is_batchnorm, kernel_size=kernel_size, padding_size=padding_size)
+        self.up = nn.Upsample(scale_factor=scale_factor, mode='trilinear')
+
+        # initialise the blocks
+        for m in self.children():
+            if m.__class__.__name__.find('UnetConv3') != -1:
+                continue
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs1, inputs2):
+        outputs2 = self.up(inputs2)
+        offset = outputs2.size()[2] - inputs1.size()[2]
+        padding = 2 * [offset // 2, offset // 2, 0]
+        outputs1 = F.pad(inputs1, padding)
+        return self.conv(torch.cat([outputs1, outputs2], 1))
