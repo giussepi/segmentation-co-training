89f7c098ea285f9a837dc09b9f649b99dde65e95
giussepi <giussepexy@gmail.com>
2022-07-24 18:45:51 +0100

    nns/models/layers/disagreement_attention/intra_class/merged.py -> MergedDABlock

--- nns/models/layers/disagreement_attention/intra_class/__init__.py
+++ nns/models/layers/disagreement_attention/intra_class/__init__.py
@@ -3,5 +3,6 @@
 
 from nns.models.layers.disagreement_attention.intra_class.combined import CombinedDABlock
 from nns.models.layers.disagreement_attention.intra_class.embedded import EmbeddedDABlock
+from nns.models.layers.disagreement_attention.intra_class.merged import MergedDABlock
 from nns.models.layers.disagreement_attention.intra_class.mixed_embedded import MixedEmbeddedDABlock
 from nns.models.layers.disagreement_attention.intra_class.standard import AttentionBlock
